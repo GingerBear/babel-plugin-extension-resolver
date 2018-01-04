e33b52f23fd908b663e84b5eb76432bceb62469c
GingerBear <bbbear444@gmail.com>
2018-01-03 22:32:24 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const resolve = require('resolve');
+const path = require('path');
+
+function resolvePath(sourcePath, currentFile, opts) {
+  if (sourcePath[0] === '.') {
+    if (sourcePath.indexOf('CommerceTokenSessionManager') > -1) debugger;
+    return resolve.sync(sourcePath, {
+      basedir: path.resolve(currentFile, '..'),
+      extensions: opts.extensions
+    });
+  } else {
+    return sourcePath;
+  }
+}
+
+function mapPathString(nodePath, state) {
+  if (!state.types.isStringLiteral(nodePath)) {
+    return;
+  }
+
+  const sourcePath = nodePath.node.value;
+  const currentFile = state.file.opts.filename;
+
+  const modulePath = resolvePath(sourcePath, currentFile, state.opts);
+  if (modulePath) {
+    if (nodePath.node.pathResolved) {
+      return;
+    }
+
+    nodePath.replaceWith(state.types.stringLiteral(modulePath));
+    nodePath.node.pathResolved = true;
+  }
+}
+
+
+const importVisitors = {
+  'ImportDeclaration|ExportDeclaration': (nodePath, state) =>{
+    mapPathString(nodePath.get('source'), state);
+  },
+};
+
+
+module.exports = ({ types }) => ({
+  name: 'extention-resolver',
+  visitor: {
+    Program: {
+      enter(programPath, state) {
+        programPath.traverse(importVisitors, state);
+      },
+    },
+  }
+});
